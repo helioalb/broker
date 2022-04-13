acc62aee64aec18bc61c3ceb6afb77b8ae3ca0dd
Helio Albano de Oliveira <helioalb@gmail.com>
2022-04-13 20:36:21 -0300

    start project

---
+++ src/test/java/br/com/mercadolivre/broker/BrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
