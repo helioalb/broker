5ab5b6fbc2fb4aee5c39c2df0801d72624272988
Helio Albano de Oliveira <helioalb@gmail.com>
2022-04-14 21:24:27 -0300

    feat(wallet): add rest endpoint to create wallet

--- src/main/java/br/com/mercadolivre/broker/wallet/infra/controller/WalletController.java
+++ src/main/java/br/com/mercadolivre/broker/wallet/infra/controller/WalletController.java
@@ -1,13 +1,19 @@
 package br.com.mercadolivre.broker.wallet.infra.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mercadolivre.broker.wallet.domain.repository.WalletRepository;
+import br.com.mercadolivre.broker.wallet.infra.controller.dto.DepositInputDto;
 import br.com.mercadolivre.broker.wallet.usecase.createwallet.CreateWallet;
 import br.com.mercadolivre.broker.wallet.usecase.createwallet.CreateWalletOutput;
+import br.com.mercadolivre.broker.wallet.usecase.deposit.Deposit;
+import br.com.mercadolivre.broker.wallet.usecase.deposit.DepositInput;
+import br.com.mercadolivre.broker.wallet.usecase.deposit.DepositOutput;
 
 @RestController
 @RequestMapping("wallets")
@@ -18,5 +24,13 @@ public class WalletController {
     @PostMapping
     public CreateWalletOutput createWallet() {
         return new CreateWallet(repository).execute();
+    }
+
+    @PostMapping(path = "/{id}/deposit")
+    public DepositOutput deposit(@RequestBody DepositInputDto deposit,
+                                 @PathVariable String id) {
+        DepositInput input =
+            new DepositInput(id, deposit.getAsset(), deposit.getAmount());
+        return new Deposit(repository).execute(input);
     }
 }
