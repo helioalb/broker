bd486a1f80c330550d87e065f0d5e6b7a5712ff8
Helio Albano de Oliveira <helioalb@gmail.com>
2022-04-24 22:10:31 -0300

    fix(trade): makes trade api pageable

--- src/main/java/br/com/mercadolivre/broker/trade/infra/controller/TradeController.java
+++ src/main/java/br/com/mercadolivre/broker/trade/infra/controller/TradeController.java
@@ -1,8 +1,8 @@
 package br.com.mercadolivre.broker.trade.infra.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +20,7 @@ public class TradeController {
     private TradeDao tradeDao;
 
     @GetMapping
-    public ResponseEntity<List<Trade>> getAll() {
-        return ResponseEntity.status(HttpStatus.OK).body(tradeDao.findAll());
+    public ResponseEntity<Page<Trade>> getAll(Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(tradeDao.findAll(pageable));
     }
 }
